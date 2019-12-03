3a11a1ce8e4cc858d1afbb36cff06f8a957a3f98
Abhishek <ab23@illinois.edu>
2019-12-03 17:11:26 -0600

    Commit 3, Permissions request completed, imageView was added to MainActivity to view the image. Comments Updated

--- app/src/main/java/com/example/mp5/LaunchActivity.java
+++ app/src/main/java/com/example/mp5/LaunchActivity.java
@@ -2,21 +2,28 @@ package com.example.mp5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
 
-public class LaunchActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LaunchActivity extends AppCompatActivity {
     /**
      * Unique gallery request code thats created when the user accesses the image gallery
      */
     public static final int GALLERY_REQUEST_CODE = 209;
-
+    /**
+     * Unique permission request identifier for the app, read perms
+     */
+    public static final int STORAGE_REQUEST_CODE = 314;
     /**
      * Function thats called when activity is started, sets up UI and calls requisite functions
      * @param savedInstanceState saved state from the previously terminated instance of this activity (unused)
@@ -29,6 +36,7 @@ public class LaunchActivity extends AppCompatActivity {
         loadImage.setOnClickListener(v -> { // Creates a handler for the button
             getImage(); // Calls getImage() if the button is clicked
         });
+        checkPerms(); // Calls checkPerms to check permissions
     }
 
     /**
@@ -65,9 +73,48 @@ public class LaunchActivity extends AppCompatActivity {
                     cursor.close();
                     // Populates an intent to start the main activity, image path loaded as an exra
                     Intent intent = new Intent(this, MainActivity.class); // Creates an empty GameActivity intent
-                    intent.putExtra("game", imgDecodableString); // Game ID is stored as an extra
+                    intent.putExtra("path", imgDecodableString); // Filepath is stored as an extra
                     startActivity(intent); // Activity is started with intent
                     finish(); // Finishes current activity
             }
+    }
+
+    /**
+     * Overrides the default permission request handler
+     * @param requestCode Request specific integer code
+     * @param permissions Array of requested permissions
+     * @param grantResults Return array of which perms were granted and denied
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case STORAGE_REQUEST_CODE: {
+                if (grantResults.length > 0 // Checks if any results were granted
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    // Does nothing if perms are granted
+                } else {
+                    finish(); // Closes app if perms are denied, perms are required for functionality
+                }
+                return;
+            }
+        }
+    }
+    public void checkPerms() {
+        List<String> permsNeeded = new ArrayList<>(); // Initializes an empty array to hold requested perms
+        // Checks if the app has read external storage permissions
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE); // If no perms, then request is added
+        }
+        // Checks if the app has write external storage permissions
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            permsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE); // If no perms, then request is added
+        }
+        if (permsNeeded.size() != 0) { // If perms are needed then the request is called
+            requestPermissions(permsNeeded.toArray(new String[permsNeeded.size()]),
+                    STORAGE_REQUEST_CODE); // Uses a result activity to call for permissions
+        }
     }
 }
--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -3,25 +3,60 @@ package com.example.mp5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * Bitmap used to hold an image such that it can be modified by other activities
+     */
+    public static Bitmap modImg;
+    /**
+     * Called when activity starts, loads image and 3 modification buttons
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Save Button
         Button save = findViewById(R.id.saveButton);
         save.setOnClickListener(v -> { // Add Image Saving section to this
             // Store Image in filesystem
             startActivity(new Intent(this, LaunchActivity.class));
             finish();
         });
-        Button restart = findViewById(R.id.restartButton);
-        restart.setOnClickListener(v -> {
+        // Restart Button
+        Button restart = findViewById(R.id.restartButton); // Grabs Restart button
+        restart.setOnClickListener(v -> { // Creates handler
+            startActivity(new Intent(this, LaunchActivity.class));
+            finish(); // If clicked, user is sent back to launch screen to choose new image
+        });
+        // Random Effect Button
+        Button randomButton = findViewById(R.id.randomButton);
+        randomButton.setOnClickListener(v -> {
             startActivity(new Intent(this, LaunchActivity.class));
             finish();
+            randomize();
         });
+        Intent intent = getIntent(); // Gets intent used to start activity
+        String filePath = intent.getStringExtra("path"); // Extracts the filepath as String
+        ImageView userImg = findViewById(R.id.imageView2); // Creates a reference for the blank imageview
+        loadImage(filePath, userImg);
+    }
+    public void loadImage(String filePath, ImageView userImg) {
+        Bitmap userBtmp = BitmapFactory.decodeFile(filePath);
+        modImg = userBtmp;
+        userImg.setImageBitmap(userBtmp);
+
+    }
+    public void randomize() {
+        return; // fix this later
     }
 }
